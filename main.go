cc1f87e929bfcff79633bd2f7c90a97af19ccf78
tlylt <liu_yongliang@hotmail.com>
2022-05-04 14:50:38 +0800

    Update

--- main.go
+++ main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hi there!")
+	// cards := newDeck()
+
+	// hand, remainingDeck := deal(cards, 5)
+
+	// hand.print()
+	// remainingDeck.print()
+
+	cards := newDeck()
+	fmt.Println(cards.toString())
 }
