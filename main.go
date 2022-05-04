41f34e7eaec47a9ad9b0a6eec5249fac35265d7d
tlylt <liu_yongliang@hotmail.com>
2022-05-04 15:41:39 +0800

    Add files

--- main.go
+++ main.go
@@ -1,15 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
-	// cards := newDeck()
-
-	// hand, remainingDeck := deal(cards, 5)
-
-	// hand.print()
-	// remainingDeck.print()
-
 	cards := newDeck()
-	fmt.Println(cards.toString())
+	cards.shuffle()
+	cards.print()
 }
