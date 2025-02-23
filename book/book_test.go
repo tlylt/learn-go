9a08ce9a00c94f7a9be535c89de571801e177168
Liu Yongliang <41845017+tlylt@users.noreply.github.com>
2025-02-23 14:26:43 +0800

    update test

--- book/book_test.go
+++ book/book_test.go
@@ -7,7 +7,6 @@ import (
 	"time"
 )
 
-// Basic test to ensure the book is created with the correct title
 func TestNewBook(t *testing.T) {
 	book := newBook("Atomic Habits")
 	if book.Title != "Atomic Habits" {
@@ -30,8 +29,28 @@ func TestRaceConditionBookNoAtomicNoLock(t *testing.T) {
 	fmt.Printf("Current page: %d\n", book.OnPage())
 	wg.Wait()
 
-	if book.OnPage() != 2 {
-		t.Error("Expected page to be 2, Actual page is ", book.OnPage())
+	if book.OnPage() != 1 {
+		t.Error("Expected page to be 1, Actual page is ", book.OnPage())
+	}
+}
+
+func TestNoRaceConditionBookAtomic(t *testing.T) {
+	t.Skip("This test is expected to fail")
+	book := newBook("Atomic Habits")
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		book.TurnPageAtomic()
+	}()
+
+	fmt.Printf("Current page: %d\n", book.OnPageAtomic())
+	wg.Wait()
+
+	if book.OnPageAtomic() != 1 {
+		t.Error("Expected page to be 1, Actual page is ", book.OnPageAtomic())
 	}
 }
 
